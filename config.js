6ee0b2a2897176c6d05b47581b4efcbc5028525f
André Santos <andrefs@cpan.org>
2015-04-22 21:23:43 +0100

    Refactoring and download script

---
+++ config.js
@@ -0,0 +1,12 @@
+
+var config  = {
+    sourcesPath       : './sources',
+    soundsPath        : './sounds',
+    tmpPath           : './tmp',
+    manifestVersion   : 'v1',
+    defaultClipMargin : '2',
+    soundEditCommand  : 'audacity'
+};
+
+
+exports.config = config;
